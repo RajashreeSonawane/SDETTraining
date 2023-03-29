5544c1102fd67944cf2899dfa884a8b7baf2b211
Rajashree Sonawane <rajashrees@synerzipune.local>
2023-03-29 16:52:41 +0530

    output attached for reverse and sorted array

---
+++ JavaBasicProgram/src/Array/ReverseArray.java
@@ -0,0 +1,56 @@
+package Array;
+
+/**
+ * problem statement:
+ * How to reverse an array in the subset of N? e.g. Input: [1,3,5,7,9,11,15,17,19], Output: [5,3,1,11,9,7,19,17,15].
+ */
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 5, 7, 9, 11, 15, 17, 19};
+        int size = 3;
+        int number = arr.length;
+        System.out.println("Original Array input is:");
+        for (int i = 0; i < number; i++)
+            System.out.println(arr[i]);
+        for (int i = 0; i < number; i += size) {
+            int left = i;
+            int right = Math.min(i + size - 1, number - 1);
+            int temp;
+            while (left < right) {
+                temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left += 1;
+                right -= 1;
+            }
+        }
+        System.out.println("Reverse Array Output is:");
+        for (int i = 0; i < number; i++)
+            System.out.println(arr[i]);
+    }
+}
+
+/**
+ * output:
+ * Original Array input is:
+ * 1
+ * 3
+ * 5
+ * 7
+ * 9
+ * 11
+ * 15
+ * 17
+ * 19
+ * Reverse Array Output is:
+ * 5
+ * 3
+ * 1
+ * 11
+ * 9
+ * 7
+ * 19
+ * 17
+ * 15
+ */
